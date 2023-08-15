264caf33e177f60bda8048dc16a3538fd6e13fdd
Kohndoe <Johnndukwe26@gmail.com>
2023-08-15 08:01:58 -0700

    Task nine

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,14 +9,13 @@ int main(void)
 int i;
 for (i = 0 ; i < 10 ; i++)
 {
-if (i == 9)
-putchar(i + '0\n');
-else
-{
 putchar(i + '0');
+if (i != 9)
+{
 putchar(',');
 putchar(' ');
 }
 }
+putchar('\n');
 return (0);
 }
